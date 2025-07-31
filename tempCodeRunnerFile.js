7b0d4939e83a67defe3917f9c5d150e8853eba3a
Pjoshi1818 <pankajmenariyajoshi@gmail.com>
2025-07-31 15:20:46 +0530

    datatype & memeory

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -35,45 +35,91 @@
 // -> string, number, boolean ,null , undefined , Bigint ,symbol;
 
 
-const mystring = "pankaj";
-const num = 123  // const num = 123.321
+// const mystring = "pankaj";
+// const num = 123  // const num = 123.321
  
-const islogedin = false
-const outsidetemp = null
-let user ;
+// const islogedin = false
+// const outsidetemp = null
+// let user ;
 
-const id =  Symbol('123')
-const sid = Symbol('123')
+// const id =  Symbol('123')
+// const sid = Symbol('123')
 
-console.log(id === sid)
+// console.log(id === sid)
 
- const bigint = 741256894512368754n   // bigint 
-
-
+//  const bigint = 741256894512368754n   // bigint 
 
 
- // ******** reference (non primitive )**********
-
- //Array , object ,function 
-
- const hero = ["kk","jj","dd"];
- {  // object
-  name :"pankaj"
-  age : 20
- }
-
- let my_obj =  {   // object
-  name : "pankaj"
-
- }
-
-let myFunction = function(){
-  console.log("function");
-
-}
 
 
-console.log(typeof myFunction)
-console.log(typeof my_obj)
-console.log(typeof hero)
-console.log(typeof id)
+//  // ******** reference (non primitive )**********
+
+//  //Array , object ,function 
+
+//  const hero = ["kk","jj","dd"];
+//  {  // object
+//   name :"pankaj"
+//   age : 20
+//  }
+
+//  let my_obj =  {   // object
+//   name : "pankaj"
+
+//  }
+
+// let myFunction = function(){
+//   console.log("function");
+
+// }
+
+
+// console.log(typeof myFunction)
+// console.log(typeof my_obj)
+// console.log(typeof hero)
+// console.log(typeof id)
+
+
+
+// ******************** memory********************
+
+// -> heap(non primitive ) and stack(primitive):
+
+// Stack (used for primitive types):
+// - Stores primitive data types like number, string, boolean, null, undefined, symbol, bigint
+// - When assigned or passed to a function, it gives a **copy**
+// - Changes to the copy do NOT affect the original
+
+// example 
+let email = "pankaj@gmail.com"
+
+
+let emailorg = email
+
+emailorg = "joshi@gmail.com"
+
+// console.log(email)
+// console.log(emailorg)
+
+
+
+
+
+// Heap (used for non-primitive types like objects, arrays, functions):
+// - Stores complex data types (non-primitives)
+// - When assigned or passed, it gives a **reference** to the same object in memory
+// - Changes to the object through one reference are visible to all references
+
+// example 
+
+let userone = {
+  email: "pankaj@gmail.com",
+  upi : "ex@adl"
+ } 
+let usertwo = userone;
+
+usertwo.email = "jayesh@gmail.com";
+
+console.log(userone)
+console.log(usertwo)
+
+
