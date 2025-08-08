0b95c22254494f8417ad903c41aa7b82e91c7dc9
Pjoshi1818 <pankajmenariyajoshi@gmail.com>
2025-08-08 19:49:33 +0530

    JSON talk

--- 02_Basics/03object_part2.js
+++ 02_Basics/03object_part2.js
@@ -55,10 +55,43 @@ const users =[
 
 // console.log(users[2].email);
 
-console.log(tinderuser)
+// console.log(tinderuser)
 
-console.log(Object.keys(tinderuser))// copey all keys  in a array 
-console.log(Object.values(tinderuser))
-console.log(Object.entries(tinderuser))
+// console.log(Object.keys(tinderuser))// copey all keys  in a array 
+// console.log(Object.values(tinderuser))
+// console.log(Object.entries(tinderuser))
 
-console.log(tinderuser.hasOwnProperty('papa'))
+// console.log(tinderuser.hasOwnProperty('papa'))
+
+
+
+
+
+// ** Destructuring and Objects ** (8/08/2025)
+
+// Creating an object named `user` with key-value pairs
+const user = {
+    id: 101,
+    username: "raja",
+    father: "rahul"
+};
+
+// Object destructuring - extracting the 'father' property from user
+const { father } = user; // father = "rahul"
+
+// Destructuring with renaming - extracting 'father' and assigning it to a new variable 'brother'
+const { father: brother } = user;
+
+console.log(brother); // Output: "rahul"
+
+// JSON format example:
+// JSON (JavaScript Object Notation) is a data format, similar to JS objects but always in string form in APIs
+// The below example is a JSON representation of an object:
+/*
+{
+    "name": "joshi",
+    "id": 121,
+    "ch": "ch",
+    "section": "pj"
+}
+*/
