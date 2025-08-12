dd9f2309a5334b4100fe7a25ce00dbc143739ba5
Pjoshi1818 <pankajmenariyajoshi@gmail.com>
2025-08-12 23:28:02 +0530

    scope in JS

--- 03_Basics/02_scope.js
+++ 03_Basics/02_scope.js
@@ -1,12 +1,13 @@
-var c= 56
-let a = 50
-if(true){
-    let a= 10
-    const b = 20
-    // var c  = 30
-    console.log("INNER : ",a)
+var c = 56       // var: function/global scoped variable
+let a = 50       // let: block scoped variable
 
+if (true) {
+    let a = 10   // new 'a' inside block scope (different from outer 'a')
+    const b = 20 // const: block scoped, value cannot be reassigned
+    // var c = 30 // would overwrite outer 'c' because var is not block scoped
+    console.log("INNER : ", a) // prints inner 'a' (10)
 }
-console.log(a)
-// console.log(b)
-// console.log(c)
+
+console.log(a) // prints outer 'a' (50)
+// console.log(b) // error: b is block scoped and not accessible here
+// console.log(c) // prints outer 'c' (56) if var c=30 inside block is commented
