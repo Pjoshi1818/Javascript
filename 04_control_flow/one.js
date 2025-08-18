3cf969abc9ab1d80ddd588158664a0a222192879
Pjoshi1818 <pankajmenariyajoshi@gmail.com>
2025-08-18 22:09:59 +0530

    Control flow in javascript

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,10 +1,12 @@
 // if else example
-const islogedin = true;
-if (islogedin) {
-    console.log("loged in :")
-} else {
-    console.log("not loged in ")
-}
+// const islogedin = true;
+// if (islogedin) {
+    // console.log("loged in :")
+// } else {
+    // console.log("not loged in ")
+// }
+
+const { is } = require("express/lib/request");
 
 // operators used in if condition
 // <, >, <=, >=, ==, ===
@@ -28,14 +30,27 @@ if (islogedin) {
 
 // else if ladder example
 let balance = 1000;
-if (balance < 600) {
-    console.log("Balance is less than 600");
-} else if (balance < 800) {
-    console.log("Balance is less than 800");
-} else if (balance < 900) {
-    console.log("Balance is less than 900");
-} else if (balance < 1200) {
-    console.log("Balance is less than 1200");
-} else {
-    console.log("Balance is greater or equal to 1200");
+// if (balance < 600) {
+//     console.log("Balance is less than 600");
+// } else if (balance < 800) {
+//     console.log("Balance is less than 800");
+// } else if (balance < 900) {
+//     console.log("Balance is less than 900");
+// } else if (balance < 1200) {
+//     console.log("Balance is less than 1200");
+// } else {
+//     console.log("Balance is greater or equal to 1200");
+// }
+
+
+const islogedin = true;
+const debitcard = true;
+const loINfromgoogle = false;
+const logedinemail = true;
+if(islogedin && debitcard){
+    console.log("allow to buy : ")
 }
+if(loINfromgoogle || logedinemail){
+    console.log("loged in ")
+}
+
