49e0d2450fd86148d36ddd6919f127b91545e75b
Pjoshi1818 <pankajmenariyajoshi@gmail.com>
2025-08-23 22:38:56 +0530

    while and Do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+// while loop 
+let i = 0;
+// while (i < 20) {
+//     console.log(` The value of i is ${i}`)
+//     i = i+2; 
+// }
+
+// const myarr = [1,2,5,8,7];
+// let index = 0;
+// while(index < myarr.length){
+//     console.log(`value of index ${index} is = ${myarr[index]}`);
+//     index++;
+// }
+
+let num = 11;
+do {
+    console.log(` num is ${num}`)
+    num++;
+    
+} while (num < 5);
